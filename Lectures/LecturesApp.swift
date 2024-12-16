4e73ba3f6a794103e47abed9fb0ad5ea931df440
bdreyerr <bendreyer21@gmail.com>
2024-12-15 20:34:55 -0800

    Initial Commit

---
+++ Lectures/LecturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LecturesApp.swift
+//  Lectures
+//
+//  Created by Ben Dreyer on 12/15/24.
+//
+
+import SwiftUI
+
+@main
+struct LecturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
