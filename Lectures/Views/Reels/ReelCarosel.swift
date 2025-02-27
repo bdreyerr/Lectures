13d979f21410babe04acdd31bfce3715a7baba43
Ben Dreyer <bendreyer21@gmail.com>
2025-02-27 12:48:04 -0800

    commit before makign the app completely free

---
+++ Lectures/Views/Reels/ReelCarosel.swift
@@ -0,0 +1,18 @@
+//
+//  ReelCarosel.swift
+//  Lectures
+//
+//  Created by Ben Dreyer on 2/22/25.
+//
+
+import SwiftUI
+
+struct ReelCarosel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReelCarosel()
+}
---
+++ Lectures/Views/Reels/ReelPlayer.swift
@@ -0,0 +1,18 @@
+//
+//  ReelPlayer.swift
+//  Lectures
+//
+//  Created by Ben Dreyer on 2/22/25.
+//
+
+import SwiftUI
+
+struct ReelPlayer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReelPlayer()
+}
